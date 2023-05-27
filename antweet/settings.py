72eab29408fd4b764c77962a68b63299e8fec37b
Yako Prihantono <yako.prihantono@spesolution.com>
2023-05-27 09:56:17 +0700

    hot fix dev

--- antweet/settings.py
+++ antweet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5674ou-fb@0otah5^g+e35p=p_*18*pyzxvpt1b9mgfp59qfy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
